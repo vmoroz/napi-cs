67571d7135a1bb544fe6fa739eff516aef2b3f54
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-09-19 16:58:37 -0700

    Add JsScope.Current

--- NApi/Types/JsScope.cs
+++ NApi/Types/JsScope.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using NApi.Exception;
 using NApi.Utils;
 
@@ -8,6 +9,8 @@ namespace NApi.Types
   {
     private JsEnv _env;
     private IntPtr? _scopePtr;
+    private JsScope? _parentScope;
+    [ThreadStatic] private static JsScope? t_current;
 
     internal JsEnv Env
     {
@@ -31,7 +34,17 @@ namespace NApi.Types
     {
       _env = env;
       _scopePtr = scopePtr;
+      _parentScope = t_current;
+      t_current = this;
     }
+
+    protected override void DisposeManaged()
+    {
+      base.DisposeManaged();
+      t_current = _parentScope;
+    }
+
+    public static JsScope? Current { get { return t_current; } }
 
     public static JsScope FromPointer(JsEnv env, IntPtr? scopePtr)
     {
