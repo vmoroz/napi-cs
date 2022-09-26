2d25f8c31cf84e28a98cabe84ce4f0ae3368bae7
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-09-26 08:59:37 -0700

    Extract JSCallbackArgs into its own file

---
+++ NApi/JSCallbackArgs.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static NApi.NodeApi;
+
+namespace NApi;
+
+public class JSCallbackArgs
+{
+  private JSValue[] _args;
+
+  public JSValue this[int index]
+  {
+    get { return _args[index]; }
+  }
+
+  public int Length
+  {
+    get { return _args.Length; }
+  }
+
+  public JSValue ThisArg { get; }
+
+  public IntPtr Data { get; }
+
+  public JSValue GetNewTarget()
+  {
+    napi_get_new_target((napi_env)Scope, CallbackInfo, out napi_value result).ThrowIfFailed();
+    return result;
+  }
+
+  internal JSValueScope Scope { get; }
+
+  internal napi_callback_info CallbackInfo { get; }
+
+  public JSCallbackArgs(JSValueScope scope, napi_callback_info callbackInfo)
+  {
+    Scope = scope;
+    CallbackInfo = callbackInfo;
+    unsafe
+    {
+      nuint argc = 0;
+      napi_get_cb_info((napi_env)scope, callbackInfo, &argc, null, null, IntPtr.Zero).ThrowIfFailed();
+      napi_value* argv = stackalloc napi_value[(int)argc];
+      napi_value thisArg;
+      IntPtr data;
+      napi_get_cb_info((napi_env)scope, callbackInfo, &argc, argv, &thisArg, new IntPtr(&data)).ThrowIfFailed();
+
+      _args = new JSValue[(int)argc];
+      for (int i = 0; i < (int)argc; ++i)
+      {
+        _args[i] = argv[i];
+      }
+
+      ThisArg = thisArg;
+      Data = data;
+    }
+  }
+}
